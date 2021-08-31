88aa5f8575dfce18a375232c9cf169611fefe360
red-drive <abhinandd.me@gmail.com>
2021-08-31 19:27:25 +0530

    Added HTML page

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,render_template
 import logging
 
 logging.basicConfig(filename="TestLog.log",format="%(asctime)s - %(levelname)s - %(message)s",level=logging.INFO)
@@ -7,9 +7,9 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     logging.info("Website Up and Running")
-    return "Welcome to the world of heroku master\n/<shopname>/<transaction>/<totalamount>/<lastsale>"
+    return render_template("index.html")
 
-@app.route("/<shopname>/<transaction>/<totalamount>/<lastsale>")
+@app.route("/<shopname>/<transaction>/<totalamount>/<lastsale>",methods=["POST"])
 def saleupdate(shopname,transaction,totalamount,lastsale):
     print(shopname+" "+transaction+" "+totalamount+" "+lastsale)
     return "200 OK"
