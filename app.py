f45c6ed8b5b73e889d0b357e944cefbb92eee7ab
red-drive <abhinandd.me@gmail.com>
2021-08-31 19:10:10 +0530

    Main Push

--- app.py
+++ app.py
@@ -7,8 +7,12 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     logging.info("Website Up and Running")
-    return "Welcome to the world of heroku master"
+    return "Welcome to the world of heroku master\n/<shopname>/<transaction>/<totalamount>/<lastsale>"
 
+@app.route("/<shopname>/<transaction>/<totalamount>/<lastsale>")
+def saleupdate(shopname,transaction,totalamount,lastsale):
+    print(shopname+" "+transaction+" "+totalamount+" "+lastsale)
+    return "200 OK"
 
 if __name__ == '__main__':
     app.run(debug=True)
